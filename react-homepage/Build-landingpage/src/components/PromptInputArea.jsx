fe9cf2aeb83a1f5ed7a093a8bb0ad1eec7cb2cd6
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-06-12 14:29:41 +0530

    done-textarea-gradiant-effect

--- react-homepage/Build-landingpage/src/components/PromptInputArea.jsx
+++ react-homepage/Build-landingpage/src/components/PromptInputArea.jsx
@@ -3,29 +3,32 @@ import submitIcon from '../assets/submit-icon.svg';
 import attachIcon from '../assets/attach-icon.svg';
 
 const PromptInputArea = () => (
-  <div className="textarea-box">
-    <div className="box1">
-      <textarea placeholder='Enter Prompt Here'></textarea>
-      <div className="model-selector">
-        <select defaultValue="model-0.2" id="model-selector">
-          <option value="model-0.1">Model 0.1</option>
-          <option value="model-0.2">Model 0.2</option>
-          <option value="model-0.3">Model 0.3</option>
-          <option value="model-0.4">Model 0.4</option>
-        </select>
+  <div className="textarea-wrapper">
+    <div className="textarea-box">
+      <div className="box1">
+        <textarea placeholder='Enter Prompt Here'></textarea>
+        <div className="model-selector">
+          <select defaultValue="model-0.2" id="model-selector">
+            <option value="model-0.1">Model 0.1</option>
+            <option value="model-0.2">Model 0.2</option>
+            <option value="model-0.3">Model 0.3</option>
+            <option value="model-0.4">Model 0.4</option>
+          </select>
+        </div>
       </div>
-    </div>
-    <div className="box2">
-      <div className="attachment-icon">
-        <img src={attachIcon} alt="Attach" className='addicon' />
-        <span className='attachment-text'>Add Attachment</span>
-      </div>
-      <div className="submit-button">
-        <span>0/1000</span>
-        <img src={submitIcon} alt="Submit" />
+      <div className="box2">
+        <div className="attachment-icon">
+          <img src={attachIcon} alt="Attach" className='addicon' />
+          <span className='attachment-text'>Add Attachment</span>
+        </div>
+        <div className="submit-button">
+          <span>0/1000</span>
+          <img src={submitIcon} alt="Submit" />
+        </div>
       </div>
     </div>
   </div>
+
 );
 
 export default PromptInputArea;
