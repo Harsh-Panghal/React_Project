76937792c730e800203b0d3e1465c9bb48e1edf3
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-06-12 18:55:10 +0530

    done-invite-card

--- react-homepage/Build-landingpage/src/App.jsx
+++ react-homepage/Build-landingpage/src/App.jsx
@@ -4,6 +4,7 @@ import Sidebar2 from './components/Sidebar2.jsx';
 import MainContent from './components/MainContent.jsx';
 import React, { useState } from 'react';
 import DeleteChatAccount from './components/DeleteChatAccount.jsx';
+import crossIcon from './assets/cross-icon.svg';
 
 
 function App() {
@@ -21,6 +22,21 @@ function App() {
       {isSidebar2Visible ? <Sidebar2 /> : <Sidebar />}
       <MainContent onToggleSidebar={handleToggleSidebar} />
       <DeleteChatAccount></DeleteChatAccount>
+      <div className="invite-card">
+        <img src={crossIcon} alt="cross-btn" />
+        <h2>Invite people</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing</p>
+        <div className='invite-btn'>
+          <button className='email'>Invite with email</button>
+          <button className='link'>Invite with link</button>
+        </div>
+        <span>Add Team</span>
+        <div className='email-input'>
+           <input type="email" placeholder='Enter Email'/>
+           <button className='send-btn'>Send</button>
+        </div>
+        <p>Lorem Ipsum is simply dummy text of the printing</p>
+      </div>
       
     </>
   );
