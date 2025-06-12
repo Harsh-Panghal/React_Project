c5be13a0d06bedfdcb3d5dcbd00f4615877aa162
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-06-12 13:26:03 +0530

    deletechat-account-card-done

--- react-homepage/Build-landingpage/src/App.jsx
+++ react-homepage/Build-landingpage/src/App.jsx
@@ -3,19 +3,23 @@ import Sidebar from './components/Sidebar.jsx';
 import Sidebar2 from './components/Sidebar2.jsx';
 import MainContent from './components/MainContent.jsx';
 import React, { useState } from 'react';
+import DeleteChatAccount from './components/DeleteChatAccount.jsx';
+
 
 function App() {
-   const [isSidebar2Visible, setIsSidebar2Visible] = useState(false); // false means Sidebar (default)
-   const [isMobile, setIsMobile] = useState(window.innerWidth <= 428);
-   
+  const [isSidebar2Visible, setIsSidebar2Visible] = useState(false); // false means Sidebar (default)
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 428);
+
 
   const handleToggleSidebar = () => {
+
     setIsSidebar2Visible(prev => !prev);
   };
   return (
     <>
       {isSidebar2Visible ? <Sidebar2 /> : <Sidebar />}
       <MainContent onToggleSidebar={handleToggleSidebar} isMobile={isMobile} />
+      <DeleteChatAccount></DeleteChatAccount>
     </>
   );
 }
