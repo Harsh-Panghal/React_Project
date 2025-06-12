9999cf1221b6477455199847376f9ddb33224388
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-06-12 11:51:03 +0530

    Woking on drop down

---
+++ react-homepage/Build-landingpage/src/components/PromptMenu.jsx
@@ -0,0 +1,28 @@
+// components/PromptMenu.jsx
+import shareIcon from '../assets/share-icon.svg';
+import editIcon from '../assets/edit-icon.svg';
+import deleteIcon from '../assets/delete-icon.svg';
+
+function PromptMenu() {
+  return (
+    <>
+      <span className="prompt">...</span>
+      <div className="prompt-description">
+        <div className="share">
+          <img src={shareIcon} alt="share" />
+          <span>Share</span>
+        </div>
+        <div className="edit-prompt">
+          <img src={editIcon} alt="edit" />
+          <span>Edit</span>
+        </div>
+        <div className="delete-prompt">
+          <img src={deleteIcon} alt="delete" />
+          <span>Delete</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PromptMenu;
