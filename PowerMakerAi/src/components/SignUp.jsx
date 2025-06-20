f4c603c495565c249595b994e007176e8cba2df4
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-06-20 18:48:27 +0530

    Verfiy-page-Interactivity-Done

--- PowerMakerAi/src/components/SignUp.jsx
+++ PowerMakerAi/src/components/SignUp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../assets/logo.svg';
 import PasswordInput from '../components/PasswordInput.jsx'; // Adjusted casing to match folder name
 
-function SignUp({ switchToSignIn }) {
+function SignUp({ switchToSignIn, switchToVerify }) {
     return (
         <div className="signIn-card">
             <img src={logo} alt="Logo" className="logo" />
@@ -12,7 +12,7 @@ function SignUp({ switchToSignIn }) {
                 <input type="email" placeholder=" Enter Email" required />
                 <PasswordInput placeholder=" Enter Password" />
                 <PasswordInput placeholder=" Confirm Password" />
-                <button type="submit" className="signIn-button">Sign Up</button>
+                <button type="submit" className="signIn-button" onClick={switchToVerify}>Sign Up</button>
             </form>
             <div className="signIn">
                 <p>Already have an account? <span onClick={switchToSignIn}>Sign In</span></p>
