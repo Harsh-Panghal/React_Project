7d32a667995c743d062fa03114320ff85b6c2159
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-06-25 13:36:02 +0530

    profilePage-responsiveness-Done

---
+++ PowerMakerAi/src/components/ChatPage.jsx
@@ -0,0 +1,53 @@
+import editTextIcon from '../assets/editText-icon.svg';
+import copyIcon from '../assets/copy-icon.svg';
+import quickPromptIcon from '../assets/quick-prompt-icon.svg';
+import arrowQuickIcon from '../assets/arrow-quick-icon.svg';
+import PromptInputArea from './PromptInputArea';
+import React, { useState } from 'react';
+
+function ChatPage({isInviteCardVisible, isDeleteChatAccountVisible}) {
+    return (
+        <div className="input-card !mt-[-30px] md:w-[734px] md:max-w-[734px] flex flex-col gap-4 ">
+            <div className="input-text-area md:w-[556px] max-w-[556px] !py-[5px] !px-[16px] bg-[#F4F4F4] rounded-md self-end">
+                <p className="text-[12px] leading-[22px] font-[400] text-[#082343]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the </p>
+            </div>
+            <div className="tools flex gap-[10px] justify-end">
+                <img src={copyIcon} alt="copy-text" className='cursor-pointer' />
+                <img src={editTextIcon} alt="edit-text" className='cursor-pointer' />
+            </div>
+            <div className="output-text text-[12px] font-[400] leading-[165%] text-[#082343] text-start">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+            <button className="show-preview self-start text-[12px] font-[400] text-[#81817C] border-1 border-[#81817C] !px-4 !py-1 rounded-md cursor-pointer">Show Preview</button>
+            <div className="border-[1px] border-[#082343] w-[732px] opacity-20 !mt-4"></div>
+            <div className="quick-prompt flex gap-2 items-center">
+                <img src={quickPromptIcon} alt="quick-prompt-icon" className='w-[24px] h-[24px]' />
+                <p className="text-[16px] font-[600] leading-[22px] text-[#082343]">Quick Prompts</p>
+            </div>
+            <div className="quick-prompt-options flext flex-col gap-2 !mt-[-10px]">
+                <div className="option flex justify-between items-center h-auto">
+                    <p className="text-[12px] font-[400] text-[#082343]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
+                    <img src={arrowQuickIcon} alt="select-prompt-option" />
+                </div>
+                <div className="border-[1px] border-[#082343] w-[732px] opacity-20"></div>
+                <div className="option flex justify-between items-center h-auto">
+                    <p className="text-[12px] font-[400] text-[#082343]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
+                    <img src={arrowQuickIcon} alt="select-prompt-option" />
+                </div>
+                <div className="border-[1px] border-[#082343] w-[732px] opacity-20"></div>
+                <div className="option flex justify-between items-center h-auto">
+                    <p className="text-[12px] font-[400] text-[#082343]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
+                    <img src={arrowQuickIcon} alt="select-prompt-option" />
+                </div>
+                <div className="border-[1px] border-[#082343] w-[732px] opacity-20"></div>
+                <div className="option flex justify-between items-center h-auto">
+                    <p className="text-[12px] font-[400] text-[#082343]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
+                    <img src={arrowQuickIcon} alt="select-prompt-option" />
+                </div>
+                <div className="border-[1px] border-[#082343] w-[732px] opacity-20"></div>
+
+            </div>
+            <PromptInputArea isDeleteChatAccountVisible={isDeleteChatAccountVisible} isInviteCardVisible={isInviteCardVisible} />
+        </div>
+    )
+}
+export default ChatPage;
--- PowerMakerAi/src/components/ProfilePage.jsx
+++ PowerMakerAi/src/components/ProfilePage.jsx
@@ -3,8 +3,10 @@ import moreIcon from '../assets/more-icon.svg';
 import tokenIcon from '../assets/token-icon.svg';
 import logo from '../assets/logo.svg';
 import React, { useState } from 'react';
+import tokenIconMobile from '../assets/token-icon-mobile.svg';
+import backIcon from '../assets/back-icon.svg';
 
-function ProfilePage({onToggleProfilePage}) {
+function ProfilePage({ onToggleProfilePage }) {
     const [name, setName] = useState('');
     const isFilled = name !== '';
     const [email, setEmail] = useState('');
@@ -24,6 +26,10 @@ function ProfilePage({onToggleProfilePage}) {
     const [Country, setCountry] = useState('');
     const isFilledCountry = Country !== '';
 
+    const [position, setPosition] = useState("position-3");
+    const [customPosition, setCustomPosition] = useState("");
+
+
     const [previewImage, setPreviewImage] = useState(null);
     const handleImageChange = (e) => {
         const file = e.target.files[0];
@@ -36,9 +42,22 @@ function ProfilePage({onToggleProfilePage}) {
 
     return (
         <>
-            <div className="profile-page gap-2 absolute top-[52%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white/90 border-1 border-solid border-[#E6E6E6CC] md:max-w-[1125px] md:w-[1125px] md:max-h-[900px] rounded-[6px] z-2 flex flex-col flex-wrap items-center justify-around shadow-md">
-                <img src={tokenIcon} alt="token area" className='absolute top-[2%] right-[2%] cursor-pointer max-w-[120px] h-auto' />
-                <h2 className="text-[#082343] font-bold">Profile</h2>
+            <div className="profile-page gap-2 w-full h-full md:h-auto absolute md:top-[52%] md:left-[50%] md:translate-x-[-50%] md:translate-y-[-50%] md:bg-white/90 bg-white border-1 border-solid border-[#E6E6E6CC] md:max-w-[1125px] md:w-[1125px] md:max-h-[900px] rounded-[6px] z-2 flex flex-col flex-wrap items-center justify-around shadow-md">
+                {/* Desktop / Tablet Image */}
+                <img
+                    src={tokenIcon}
+                    alt="token area"
+                    className="token-icon md:block absolute top-[2%] right-[2%]  cursor-pointer max-w-[120px] h-auto"
+                />
+
+                {/* Mobile Image */}
+                <img
+                    src={tokenIconMobile}
+                    alt="mobile token area"
+                    className="block md:hidden absolute top-[2%] right-[2%] cursor-pointer max-w-[100px] w-[70px]"
+                />
+                <img src={backIcon} alt="backicon" className="md:hidden absolute top-[2%] left-[2%] cursor-pointer w-[20px]" />
+                <h2 className="text-[#082343] font-bold !mt-[1.2rem]">Profile</h2>
                 <input type="file" id="profile-pic" accept="image/*" className='hidden' onChange={handleImageChange} />
                 <label htmlFor="profile-pic" className=' text-center cursor-pointer'>
                     {previewImage ? (
@@ -60,7 +79,7 @@ function ProfilePage({onToggleProfilePage}) {
                             <span class="absolute left-0 bottom-0 w-0 h-[1px] bg-[#0E5185] transition-all duration-300 group-hover:w-full" /></span>
                     )}
                 </label>
-                <div className=' profile-input-area text-[16px] max-w-[897px] h-auto flex gap-6 justify-between flex-wrap ' >
+                <div className=' profile-input-area  text-[16px] max-w-[897px] h-auto flex flex-col items-center md:flex-row gap-6 md:justify-between flex-wrap ' >
                     <div className="relative w-[400px] ">
                         <input
                             type="text"
@@ -68,13 +87,13 @@ function ProfilePage({onToggleProfilePage}) {
                             placeholder=" "
                             value={name}
                             onChange={(e) => setName(e.target.value)}
-                            className="peer w-full min-h-[56px] pt-5 pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] outline-none transition-all"
+                            className="peer w-full min-h-[48px] md:min-h-[56px] !pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] outline-none transition-all"
                         />
                         <label
                             htmlFor="name"
                             className={`
           absolute left-6 text-[#81817C]  bg-white/80 transition-all duration-200 ease-in 
-          ${isFilled ? 'top-[-0.7rem] text-[14px]' : 'top-4 text-[16px]'} 
+          ${isFilled ? 'top-[-0.7rem] text-[14px]' : 'top-3 text-[16px]'} 
           peer-focus:top-2 peer-focus:text-[14px]
         `}
                         >
@@ -88,13 +107,13 @@ function ProfilePage({onToggleProfilePage}) {
                             placeholder=" "
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            className="peer w-full min-h-[56px] pt-5 pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
+                            className="peer w-full min-h-[48px] md:min-h-[56px] !pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
                         />
                         <label
                             htmlFor="email"
                             className={`
-          absolute left-6 text-[#81817C]  bg-[#ebf7fa]  transition-all duration-200 ease-in 
-          ${isFilledemail ? 'top-[-0.7rem] text-[14px]' : 'top-4 text-[16px]'} 
+          absolute left-6 text-[#81817C]  md:bg-[#ebf7fa] bg-white  transition-all duration-200 ease-in 
+          ${isFilledemail ? 'top-[-0.7rem] text-[14px]' : 'top-3 text-[16px]'} 
           peer-focus:top-2 peer-focus:text-[14px]
         `}
                         >
@@ -108,32 +127,57 @@ function ProfilePage({onToggleProfilePage}) {
                             placeholder=" "
                             value={Cname}
                             onChange={(e) => setCName(e.target.value)}
-                            className="peer w-full min-h-[56px] pt-5 pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] outline-none transition-all"
+                            className="peer w-full min-h-[48px] md:min-h-[56px] !pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] outline-none transition-all"
                         />
                         <label
                             htmlFor="name"
                             className={`
           absolute left-6 text-[#81817C]  bg-white/80  transition-all duration-200 ease-in 
-          ${isFilledCname ? 'top-[-0.7rem] text-[14px]' : 'top-4 text-[16px]'} 
+          ${isFilledCname ? 'top-[-0.7rem] text-[14px]' : 'top-3 text-[16px]'} 
           peer-focus:top-2 peer-focus:text-[14px]
         `}
                         >
                             Company Name
                         </label>
                     </div>
-                    <div className=' max-w-[400px] position-area relative'>
-                        <div className='text-[rgba(0, 20, 45, 1)] text-[14px] absolute top-[-0.7rem] bg-[#F3FAFD]   left-6'>Position</div>
-                        <img src={moreIcon} alt="option-selector" className='absolute left-[92%] top-[45%] w-[24px] h-[24px] cursor-pointer' />
-                        <select name="position" id="position" className=' cursor-pointer w-[400px] focus:outline-none appearance-none min-h-[56px] text-[16px] text-[#082343] border-1 border-solid border-[#08234333] !pl-6 rounded-[6px]'>
+                    <div className="max-w-[400px] position-area relative">
+                        <div className="text-[rgba(0, 20, 45, 1)] text-[14px] absolute top-[-0.7rem] bg-[#F3FAFD] left-6">
+                            Position
+                        </div>
+
+                        <img
+                            src={moreIcon}
+                            alt="option-selector"
+                            className="absolute left-[92%] md:top-[30%] top-[25%] w-[24px] h-[24px] cursor-pointer"
+                        />
+
+                        <select
+                            name="position"
+                            id="position"
+                            value={position}
+                            onChange={(e) => setPosition(e.target.value)}
+                            className="cursor-pointer w-[400px] focus:outline-none appearance-none min-h-[48px] md:min-h-[56px] text-[16px] text-[#082343] border border-[#08234333] !pl-6 rounded-[6px]"
+                        >
                             <option value="position-1">CEO</option>
                             <option value="position-2">CTO</option>
-                            <option value="position-3" selected>Manager</option>
-                            <option value="other">other</option>
+                            <option value="position-3">Manager</option>
+                            <option value="other">Other</option>
                         </select>
+
+                        {/* Conditionally Render Custom Input Field */}
                     </div>
-                    <div className='address-area w-[100%] max-w-[100%]'>
+                    {position === "other" && (
+                            <input
+                                type="text"
+                                value={customPosition}
+                                onChange={(e) => setCustomPosition(e.target.value)}
+                                placeholder="Enter your position"
+                                className="justify-end !ml-auto w-[400px] min-h-[48px] md:min-h-[56px] pt-5 !pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
+                            />
+                        )}
+                    <div className='address-area  w-[100%] max-w-[100%]'>
                         {/* <div className='text-[rgba(0, 20, 45, 1)] text-[14px] opacity-50'>Address</div> */}
-                        <div className="address-wrapper flex justify-between flex-wrap gap-4">
+                        <div className="address-wrapper flex flex-col items-center md:flex-row md:justify-between flex-wrap gap-4">
                             <div className="relative w-[400px]">
                                 <input
                                     type="text"
@@ -141,13 +185,13 @@ function ProfilePage({onToggleProfilePage}) {
                                     placeholder=" "
                                     value={AddressLine1}
                                     onChange={(e) => setAddressLine1(e.target.value)}
-                                    className="peer w-full min-h-[56px] pt-5 pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
+                                    className="peer w-full min-h-[48px] md:min-h-[56px] pt-5 !pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
                                 />
                                 <label
                                     htmlFor="add1"
                                     className={`
           absolute left-6 text-[#81817C] bg-white/80 transition-all duration-200 ease-in 
-          ${isFilledAddressLine1 ? 'top-[-0.7rem] text-[14px]' : 'top-4 text-[16px]'} 
+          ${isFilledAddressLine1 ? 'top-[-0.7rem] text-[14px]' : 'top-3 text-[16px]'} 
           peer-focus:top-2 peer-focus:text-[14px]
         `}
                                 >
@@ -161,59 +205,66 @@ function ProfilePage({onToggleProfilePage}) {
                                     placeholder=" "
                                     value={AddressLine2}
                                     onChange={(e) => setAddressLine2(e.target.value)}
-                                    className="peer w-full min-h-[56px] pt-5 pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
+                                    className="peer w-full min-h-[48px] md:min-h-[56px] pt-5 !pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
                                 />
                                 <label
                                     htmlFor="add2"
                                     className={`
           absolute left-6 text-[#81817C]  bg-[#eaf3f52b]  transition-all duration-200 ease-in 
-          ${isFilledAddressLine2 ? 'top-[-0.7rem] text-[14px]' : 'top-4 text-[16px]'} 
+          ${isFilledAddressLine2 ? 'top-[-0.7rem] text-[14px]' : 'top-3 text-[16px]'} 
           peer-focus:top-2 peer-focus:text-[14px]
         `}
                                 >
                                     Address Line 2
                                 </label>
                             </div>
-                            <div className="relative w-[400px]">
-                                <input
-                                    type="text"
-                                    id="city"
-                                    placeholder=" "
-                                    value={City}
-                                    onChange={(e) => setCity(e.target.value)}
-                                    className="peer w-full min-h-[56px] pt-5 pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
-                                />
-                                <label
-                                    htmlFor="city"
-                                    className={`
-          absolute left-6 text-[#81817C] bg-white/80 transition-all duration-200 ease-in 
-          ${isFilledCity ? 'top-[-0.7rem] text-[14px]' : 'top-4 text-[16px]'} 
-          peer-focus:top-2 peer-focus:text-[14px]
-        `}
-                                >
-                                    City
-                                </label>
+
+                            <div className="flex gap-2 md:justify-between  flex-wrap w-[100%] justify-center ">
+                                {/* City Field */}
+                                <div className="relative md:w-[400px] w-[48%]">
+                                    <input
+                                        type="text"
+                                        id="city"
+                                        placeholder=" "
+                                        value={City}
+                                        onChange={(e) => setCity(e.target.value)}
+                                        className="peer w-full min-h-[48px] md:min-h-[56px] pt-5 !pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
+                                    />
+                                    <label
+                                        htmlFor="city"
+                                        className={`
+        absolute left-6 text-[#81817C] bg-white/80 transition-all duration-200 ease-in 
+        ${isFilledCity ? 'top-[-0.7rem] text-[14px]' : 'top-3 text-[16px]'} 
+        peer-focus:top-2 peer-focus:text-[14px]
+      `}
+                                    >
+                                        City
+                                    </label>
+                                </div>
+
+                                {/* Zip Code Field */}
+                                <div className="relative md:w-[400px] w-[48%]">
+                                    <input
+                                        type="text"
+                                        id="zip"
+                                        placeholder=" "
+                                        value={Zip}
+                                        onChange={(e) => setZip(e.target.value)}
+                                        className="peer w-full  min-h-[48px] md:min-h-[56px] pt-5 !pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
+                                    />
+                                    <label
+                                        htmlFor="zip"
+                                        className={`
+        absolute left-6 text-[#81817C] bg-white/80 transition-all duration-200 ease-in 
+        ${isFilledZip ? 'top-[-0.7rem] text-[14px]' : 'top-3 text-[16px]'} 
+        peer-focus:top-2 peer-focus:text-[14px]
+      `}
+                                    >
+                                        Zip Code
+                                    </label>
+                                </div>
                             </div>
-                            <div className="relative w-[400px]">
-                                <input
-                                    type="text"
-                                    id="zip"
-                                    placeholder=" "
-                                    value={Zip}
-                                    onChange={(e) => setZip(e.target.value)}
-                                    className="peer w-full min-h-[56px] pt-5 pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
-                                />
-                                <label
-                                    htmlFor="zip"
-                                    className={`
-          absolute left-6 text-[#81817C] bg-white/80 transition-all duration-200 ease-in 
-          ${isFilledZip ? 'top-[-0.7rem] text-[14px]' : 'top-4 text-[16px]'} 
-          peer-focus:top-2 peer-focus:text-[14px]
-        `}
-                                >
-                                    Zip Code
-                                </label>
-                            </div>
+
                             <div className="relative w-[400px]">
                                 <input
                                     type="text"
@@ -221,7 +272,7 @@ function ProfilePage({onToggleProfilePage}) {
                                     placeholder=" "
                                     value={Country}
                                     onChange={(e) => setCountry(e.target.value)}
-                                    className="peer w-full min-h-[56px] pt-5 pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
+                                    className="peer w-full min-h-[48px] md:min-h-[56px] pt-5 !pl-6 text-[#082343] text-[16px] border border-[#08234333] rounded-[6px] focus:outline-none transition-all"
                                 />
                                 <label
                                     htmlFor="country"
@@ -244,7 +295,7 @@ function ProfilePage({onToggleProfilePage}) {
                 </span>
                 <img src={logo} alt="logo" className='absolute top-[2%] left-[36%] w-[25%] opacity-10 blur-[15px] z-[-1]' />
             </div>
-            
+
         </>
     )
 }
