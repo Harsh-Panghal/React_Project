2700f8ce47f865a6358a6b82909f261ae1552a88
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-06-27 16:47:06 +0530

    RelationshipTable-done

---
+++ PowerMakerAi/src/components/RelationshipTable.jsx
@@ -0,0 +1,27 @@
+
+function RelationshipTable() {
+    return (
+        <div className="relationship-area md:w-[734px] md:max-w-[734px] flex flex-col gap-6">
+            <h2 className="text-[18px] text-[#082343] font-[600]">Relationships</h2>
+            <div className="relationship-table rounded-md overflow-hidden shadow-[0px_2px_4px_0px_#00000014] border border-[#E6E6E6]">
+                <table className="w-full table-fixed border-separate border-spacing-0">
+                    <thead className='bg-[#F4F4F4] h-[41px]'>
+                        <tr>
+                            <th className=" text-[14px] font-[400] text-[#81817C] text-left border border-[#E6E6E6] !px-4">Target Entity</th>
+                            <th className="w-[160px] text-[14px] font-[400] text-[#81817C] text-left border border-[#E6E6E6] !px-4">Referencing Entity </th>
+                            <th className="text-[14px] font-[400] text-[#81817C] text-left border border-[#E6E6E6] !px-4">Schema Name</th>
+                            <th className="text-[14px] font-[400] text-[#81817C] text-left border border-[#E6E6E6] !px-4">Display Name</th>
+                            <th className="text-[14px] font-[400] text-[#81817C] text-left border border-[#E6E6E6] !px-4">Required Level</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="bg-white h-[42px] ">
+                            <td colSpan={5} className='text-[14px] font-[400] text-[#81817C] border border-[#E6E6E6] !px-7 !py-3.5'>No relationship data found</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+export default RelationshipTable;
