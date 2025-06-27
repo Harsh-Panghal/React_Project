38314fe10c8b55798766743c4b835154c655e22f
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-06-27 17:00:26 +0530

    Entity-attribute-relationship-table come-when we hit showPreview btn

--- PowerMakerAi/src/components/ChatPageWithTable.jsx
+++ PowerMakerAi/src/components/ChatPageWithTable.jsx
@@ -10,8 +10,9 @@ import PromptInputArea from './PromptInputArea';
 
 
 function ChatPageWithTable({ isDeleteChatAccountVisible, isInviteCardVisible }) {
+    const [isShowPreviewClicked, setIsShowPreviewClicked] = useState(false);
     return (
-        <div className="input-card2 overflow-auto hide-scrollbar md:max-h-[560px]  border border-red-400  !mb-auto relative  !mt-[-30px] md:w-[734px] md:max-w-[734px] flex flex-col gap-4   ">
+        <div className={`input-card2 overflow-auto hide-scrollbar md:max-h-[560px]  relative  md:w-[734px] md:max-w-[734px] flex flex-col gap-4 ${isShowPreviewClicked ? '!mt-[-30px] !mb-auto' : ''}`}>
             <div className="input-text-area flex gap-[5px] !ml-auto">
                 <div className=" md:w-content max-w-[556px] !py-[14px] !px-[16px] bg-[#F4F4F4] rounded-md ">
                     <p className="text-[12px] leading-[22px] font-[400] text-[#082343]">Extract all the collected plugin fillter details</p>
@@ -26,15 +27,15 @@ function ChatPageWithTable({ isDeleteChatAccountVisible, isInviteCardVisible }) 
                 <img src={logo} className="w-[28px] h-[35px]" alt="" />
                 <div className="flex flex-col gap-2">
                     <p className="text-[14px] font-[400] leading-[165%] text-[#082343] text-start">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
-                    <button className="show-preview self-start text-[12px] font-[400] text-[#81817C] border-1 border-[#81817C] !px-4 !py-1 rounded-md cursor-pointer hover:bg-[#b9b9b8] hover:text-[#082343] trabsition-all duration-300">Show Preview</button>
+                    <button className="show-preview self-start text-[12px] font-[400] text-[#81817C] border-1 border-[#81817C] !px-4 !py-1 rounded-md cursor-pointer hover:bg-[#b9b9b8] hover:text-[#082343] trabsition-all duration-300" onClick={() => setIsShowPreviewClicked(!isShowPreviewClicked)}>Show Preview</button>
                 </div>
             </div>
             {/* ------------------Entity Table------------------- */}
-            <EntityTable />
+            {isShowPreviewClicked && <EntityTable />}
             {/* ------------------Attribute Table------------------- */}
-            <AttributeTable />
+            {isShowPreviewClicked && <AttributeTable />}
             {/* ------------------------Relationship Table---------------- */}
-            <RelationshipTable />
+            {isShowPreviewClicked && <RelationshipTable />}
 
             <PromptInputArea isDeleteChatAccountVisible={isDeleteChatAccountVisible} isInviteCardVisible={isInviteCardVisible} />
         </div>
