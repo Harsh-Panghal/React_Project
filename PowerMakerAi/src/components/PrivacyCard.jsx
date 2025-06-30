c6c3f91adf7b19dec80e67a10e2b627582f298c0
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-06-30 19:04:52 +0530

    Done

--- PowerMakerAi/src/components/PrivacyCard.jsx
+++ PowerMakerAi/src/components/PrivacyCard.jsx
@@ -1,7 +1,7 @@
 
 function PrivacyCard() {
     return (
-        <div className="terms-condition-card md:w-[1200px] max-w[1200px] bg-white border border-[#E6E6E6CC] absolute top-[90px] left-[274px] !px-8 !py-6 rounded-md shadow-[0px_4px_12px_rgba(0,0,0,0.1)]">
+        <div className="terms-condition-card md:w-[1200px] max-w[1200px] bg-white border border-[#E6E6E6CC] absolute top-[90px] left-[274px] !px-8 !py-6 rounded-md shadow-[0px_4px_12px_rgba(0,0,0,0.1)] z-60">
             <h2 className="text-[20px] font-[600] text-[#082343]">Privacy Policy</h2>
             <p className="text-[16px] font-[500] text-[#81817C] capitalize leading-[22px] !mt-3 text-start"> Lorem ipsum dolor sit amet. Id corporis voluptatem ad odio adipisci aut quasi accusamus et quidem cupiditate! Vel aperiam doloremque et perspiciatis nihil qui inventore maxime quo galisum maxime qui harum nihil ut alias quia vel velit voluptate? Eos vero praesentium ab commodi veritatis ut cupiditate commodi qui ipsa natus. Et voluptas internos sit dolores possimus in repellat sint sit velit dolor.<br />
                 Est ducimus dicta est distinctio architecto id possimus natus sed reiciendis unde ut cupiditate accusamus. Aut praesentium sint aut possimus dolorem ut atque culpa sit incidunt nemo eos atque perspiciatis sed incidunt delectus! Nam galisum deserunt et esse eligendi et veniam dolores est rerum perferendis.<br />
